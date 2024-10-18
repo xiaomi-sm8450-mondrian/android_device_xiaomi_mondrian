98d036051cb956887d4dfe6067d9733f145acf67
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    mondrian: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_mondrian.mk
+++ lineage_mondrian.mk
@@ -16,14 +16,10 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := 23013PC75G
 
-PRODUCT_SYSTEM_NAME := mondrian_global
-PRODUCT_SYSTEM_DEVICE := mondrian
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="mondrian_global-user 14 UKQ1.230804.001 V816.0.10.0.UMNMIXM release-keys" \
-    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
-
-BUILD_FINGERPRINT := POCO/mondrian_global/mondrian:14/UKQ1.230804.001/V816.0.10.0.UMNMIXM:user/release-keys
+    BuildDesc="mondrian_global-user 14 UKQ1.230804.001 V816.0.10.0.UMNMIXM release-keys" \
+    BuildFingerprint=POCO/mondrian_global/mondrian:14/UKQ1.230804.001/V816.0.10.0.UMNMIXM:user/release-keys \
+    DeviceProduct=mondrian \
+    SystemName=mondrian_global
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
