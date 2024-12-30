6c96d40c3949ab0cc7f2e87695b0e28718a30d67
keosh1 <conquer2152016@gmail.com>
2024-12-30 15:01:54 +0000

    Initialize for Evolution X 10.x

--- lineage_mondrian.mk
+++ lineage_mondrian.mk
@@ -10,6 +10,13 @@ $(call inherit-product, device/xiaomi/mondrian/device.mk)
 # Inherit from common lineage configuration
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+WITH_GMS := true
+TARGET_ENABLE_BLUR := true
+PRODUCT_NO_CAMERA := false
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+#TARGET_DEFAULT_PIXEL_LAUNCHER := false
+
 PRODUCT_NAME := lineage_mondrian
 PRODUCT_DEVICE := mondrian
 PRODUCT_MANUFACTURER := Xiaomi
@@ -27,19 +34,3 @@ PRODUCT_SYSTEM_DEVICE := mondrian
 BUILD_FINGERPRINT := POCO/mondrian_global/mondrian:14/UKQ1.230804.001/V816.0.10.0.UMNMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# RisingOS stuff
-PRODUCT_BUILD_PROP_OVERRIDES += \
-RisingChipset="Snapdragon 8+ Gen1" \
-RisingMaintainer="keosh"
-RISING_PACKAGE_TYPE := GAPPS
-TARGET_CORE_GMS := true
-TARGET_CORE_GMS_EXTRAS := true
-TARGET_ENABLE_BLUR := true
-PRODUCT_NO_CAMERA := false
-TARGET_HAS_UDFPS := true
-EXTRA_UDFPS_ANIMATIONS := true
-# RisingOS GMS build flags
-WITH_GMS := true
-TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
-TARGET_DEFAULT_PIXEL_LAUNCHER := true
