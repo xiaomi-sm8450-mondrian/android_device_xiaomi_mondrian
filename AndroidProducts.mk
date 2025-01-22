1a482baa63792160079ab9c76fe3db3e4c696d7a
keosh1 <106485828+keosh1@users.noreply.github.com>
2025-01-22 18:05:35 +0000

    derpify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_mondrian.mk
+    $(LOCAL_DIR)/derp_mondrian.mk
--- derp_mondrian.mk
+++ derp_mondrian.mk
@@ -8,9 +8,19 @@
 $(call inherit-product, device/xiaomi/mondrian/device.mk)
 
 # Inherit from common lineage configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_mondrian
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_CORE_GMS_EXTRAS := true
+TARGET_ENABLE_BLUR := true
+PRODUCT_NO_CAMERA := false
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_CALL_RECORDING := true
+
+PRODUCT_NAME := derp_mondrian
 PRODUCT_DEVICE := mondrian
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -23,19 +33,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     SystemName=mondrian_global
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# RisingOS stuff
-PRODUCT_BUILD_PROP_OVERRIDES += \
-RisingChipset="Snapdragon 8+ Gen1" \
-RisingMaintainer="keosh"
-RISING_PACKAGE_TYPE := GAPPS
-TARGET_CORE_GMS := true
-TARGET_CORE_GMS_EXTRAS := true
-TARGET_ENABLE_BLUR := true
-PRODUCT_NO_CAMERA := false
-TARGET_HAS_UDFPS := true
-EXTRA_UDFPS_ANIMATIONS := true
-# RisingOS GMS build flags
-WITH_GMS := true
-TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
-TARGET_DEFAULT_PIXEL_LAUNCHER := true
