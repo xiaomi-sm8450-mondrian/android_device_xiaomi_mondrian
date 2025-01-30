248f7f7903ee90a55034c1f74f975ebd7ea2d231
keosh <conquer215206@gmail.com>
2025-01-30 23:08:08 +0000

    mondrian: Silence some spammy logging
    HWC and cit sensor logspam is so intense it bumps up logd's idle CPU
    usage up to over 3%. With this change, logs are much cleaner and logd
    idle CPU usage is reduced to under 1%, while still allowing error logs
    to pass through.
    - Do this only in user/debug builds (except eng).
    - Add wifi and cnss too while we're at it, they can get noisy at times.

--- device.mk
+++ device.mk
@@ -21,6 +21,26 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/resourcemanager_waipio_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape/resourcemanager_waipio_mtp.xml \
     $(LOCAL_PATH)/audio/usecaseKvManager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usecaseKvManager.xml
 
+# Logging
+SPAMMY_LOG_TAGS := \
+    MiStcImpl \
+    SDM \
+    SDM-histogram \
+    SRE \
+    WifiHAL \
+    cnss-daemon \
+    libcitsensorservice@2.0-impl \
+    libsensor-displayalgo \
+    libsensor-parseRGB \
+    libsensor-ssccalapi \
+    sensors \
+    vendor.qti.hardware.display.composer-service \
+    vendor.xiaomi.sensor.citsensorservice@2.0-service
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_VENDOR_PROPERTIES += \
+    $(foreach tag,$(SPAMMY_LOG_TAGS),log.tag.$(tag)=E)
+endif
+
 # Overlay
 PRODUCT_PACKAGES += \
     ApertureResMondrian \
